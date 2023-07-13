73d6c322295f13b23cdba98205234e77d01534d4
Richard Bown <richard.bown.ext@zeiss.com>
2023-07-13 13:34:14 +0200

    Initial updates

--- extractor.py
+++ extractor.py
@@ -1,10 +1,15 @@
 from azure.devops.connection import Connection
 from msrest.authentication import BasicAuthentication
 import pprint
+from configparser import ConfigParser
+
+# config
+config = ConfigParser()
+config.read('user.ini')
 
 # Fill in with your personal access token and org URL
-personal_access_token = 'YOURPAT'
-organization_url = 'https://dev.azure.com/YOURORG'
+personal_access_token = config.get('section_main', 'PAT')
+organization_url = config.get('section_main', 'ADO_URL')
 
 # Create a connection to the org
 credentials = BasicAuthentication('', personal_access_token)
@@ -13,18 +18,24 @@ connection = Connection(base_url=organization_url, creds=credentials)
 # Get a client (the "core" client provides access to projects, teams, etc)
 core_client = connection.clients.get_core_client()
 
+project_collections = core_client.get_project_collections()
+
 # Get the first page of projects
 get_projects_response = core_client.get_projects()
-index = 0
-while get_projects_response is not None:
-    for project in get_projects_response.value:
-        pprint.pprint("[" + str(index) + "] " + project.name)
-        index += 1
-    if get_projects_response.continuation_token is not None and get_projects_response.continuation_token != "":
-        # Get the next page of projects
-        get_projects_response = core_client.get_projects(continuation_token=get_projects_response.continuation_token)
-    else:
-        # All projects have been retrieved
-        get_projects_response = None
+get_teams_response = core_client.get_all_teams()
+
+for project in get_projects_response:
+    pprint.pprint("Project = " + project.name + " (id = " + project.id + ")")
+
+    #project_stuff = core_client.get_project(project.id)
+    #pprint.pprint(project_stuff)
+
+    for team in get_teams_response:
+        pprint.pprint("Team = " + team.name + " (id = " + team.id + ")")
+
+        connected_services = core_client.get_connected_services(project.id)
+        #team_members = core_client.get_team_members_with_extended_properties(project.id, team.id)
+
+        pprint.pprint(connected_services)
 
 
