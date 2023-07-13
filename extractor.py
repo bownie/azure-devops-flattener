c82d62f7c1d231e67e9c367f6d15b4b96da26b36
Richard Bown <richard.bown.ext@zeiss.com>
2023-07-13 14:33:53 +0200

    Starting to explore

--- extractor.py
+++ extractor.py
@@ -2,6 +2,7 @@ from azure.devops.connection import Connection
 from msrest.authentication import BasicAuthentication
 import pprint
 from configparser import ConfigParser
+from azure.devops.v7_1.work import TeamContext 
 
 # config
 config = ConfigParser()
@@ -15,8 +16,11 @@ organization_url = config.get('section_main', 'ADO_URL')
 credentials = BasicAuthentication('', personal_access_token)
 connection = Connection(base_url=organization_url, creds=credentials)
 
-# Get a client (the "core" client provides access to projects, teams, etc)
+# set up the clients
 core_client = connection.clients.get_core_client()
+work_item_tracking_client = connection.clients.get_work_item_tracking_client()
+feature_management_client = connection.clients.get_feature_management_client()
+work_client = connection.clients.get_work_client()
 
 project_collections = core_client.get_project_collections()
 
@@ -33,9 +37,23 @@ for project in get_projects_response:
     for team in get_teams_response:
         pprint.pprint("Team = " + team.name + " (id = " + team.id + ")")
 
-        connected_services = core_client.get_connected_services(project.id)
+        #connected_services = core_client.get_connected_services(project.id)
+        #pprint.pprint(connected_services)
+
         #team_members = core_client.get_team_members_with_extended_properties(project.id, team.id)
 
-        pprint.pprint(connected_services)
+        team_context =  TeamContext(project.name, project.id, team.name, team.id)
 
+        try:
+            backlog_configuration = work_client.get_backlog_configurations(team_context)
+            pprint.pprint("    " + backlog_configuration)
+        except:
+            pprint.pprint("  Can't get Backlog Configuration: no permission")
+
+        for work_item_type in work_item_tracking_client.get_work_item_types(project.id):
+            print("  Work Item Type = " + work_item_type.name + " (ref name = " + work_item_type.reference_name + ")")
+
+
+#for feature in feature_management_client.get_features():
+#    pprint.pprint("Feature = " + feature.name)
 
