6e611838d2200978d488f1e75665906d57703dbc
Richard Bown <rich@xyglo.com>
2023-07-15 20:30:38 +0200

    Warnings

--- extractor.py
+++ extractor.py
@@ -1,8 +1,8 @@
-from azure.devops.connection import Connection
-from msrest.authentication import BasicAuthentication
 import pprint
 from configparser import ConfigParser
 from azure.devops.v7_1.work import TeamContext 
+from azure.devops.connection import Connection
+from msrest.authentication import BasicAuthentication
 
 # config
 config = ConfigParser()
@@ -33,16 +33,8 @@ get_teams_response = core_client.get_all_teams()
 for project in get_projects_response:
     pprint.pprint("Project = " + project.name + " (id = " + project.id + ")")
 
-    #project_stuff = core_client.get_project(project.id)
-    #pprint.pprint(project_stuff)
-
     for team in get_teams_response:
         pprint.pprint("Team = " + team.name + " (id = " + team.id + ")")
-
-        #connected_services = core_client.get_connected_services(project.id)
-        #pprint.pprint(connected_services)
-
-        #team_members = core_client.get_team_members_with_extended_properties(project.id, team.id)
 
         team_context =  TeamContext(project.name, project.id, team.name, team.id)
 
@@ -53,9 +45,6 @@ for project in get_projects_response:
             pprint.pprint("  Can't get Backlog Configuration: no permission")
 
         for work_item_type in work_item_tracking_client.get_work_item_types(project.id):
-            print("  Work Item Type = " + work_item_type.name + " (ref name = " + work_item_type.reference_name + ")")
-
-
-#for feature in feature_management_client.get_features():
-#    pprint.pprint("Feature = " + feature.name)
-
+            print("  Work Item Type = " + work_item_type.name +
+                  " (ref name = " + work_item_type.reference_name + ")")
+            
--- organization/user.py
+++ organization/user.py
@@ -1,5 +1,3 @@
 
 class User:
     user_name: str
-
-
