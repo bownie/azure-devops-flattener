de765c1f624c000b1f5b27e8eb4b65b60119ec34
Richard Bown <rich@xyglo.com>
2023-07-17 07:28:04 +0200

    Goodness

--- tests/test_organization.py
+++ tests/test_organization.py
@@ -4,12 +4,12 @@ import organization.organization
 ORGANIZATION_NAME = "Test Organization"
 org = organization.organization.Organization(ORGANIZATION_NAME)
 
-def test_exception():
+def raise_exception():
     raise SystemExit(1)
 
 def test_mytest():
     with pytest.raises(SystemExit):
-        test_exception()
+        raise_exception()
 
 def test_organization():
     assert org.get_name() == ORGANIZATION_NAME
